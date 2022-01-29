67e0440d50acc7c839a690affa88c5ec74bffa24
mneu windows <alberto.harres@gmail.com>
2022-01-30 00:09:17 +0100

    tweaks

--- arche-reading.py
+++ arche-reading.py
@@ -11,7 +11,7 @@ from playsound import playsound
 alphabet = list('撒健億媒間増感察総負街時哭병体封列効你老呆安发は切짜확로감外年와모ゼДが占乜산今もれすRビコたテパアEスどバウПm가бうクん스РりwАêãХйてシжغõ小éजভकöলレ入धबलخFসeवমوযиथशkحくúoनবएদYンदnuনمッьノкتبهtт一ادіاгرزरjvةзنLxっzэTपнлçşčतلイयしяトüषখথhцहیরこñóহリअعसमペيフdォドрごыСいگдとナZকইм三ョ나gшマで시Sقに口س介Иظ뉴そキやズВ자ص兮ض코격ダるなф리Юめき宅お世吃ま来店呼설진음염론波密怪殺第断態閉粛遇罩孽關警')
 
 PyAudio = pyaudio.PyAudio     
-SAMPLERATE = 4000
+SAMPLERATE = 8000
 
 def openSampleData ():
   with open('sample_data/354.txt', encoding='utf-8') as f:
@@ -38,6 +38,7 @@ def playDetections(detections):
 # play wavedata as a string of chr(d[4])
 def playWavedata (numbers, sample_rate=SAMPLERATE):
   wavedata = np.asarray(numbers).astype(np.int8)
+
   write("temp.wav", sample_rate, wavedata)
   playsound('temp.wav')
 
